095ec490487e50efb92b1d0dc749cc342db9977d
dr_bonzo <dr_bonzo@northslope.pl>
2016-06-01 19:05:44 +0200

    Added AbstractWorkflowTransition with defaults for WorkflowTransition implementation

---
+++ src/NorthslopePL/Workflow/AbstractWorkflowTransition.php
@@ -0,0 +1,42 @@
+<?php
+namespace NorthslopePL\Workflow;
+
+/**
+ * Use this trait in your class to simplify it's code by using default configuration.
+ */
+trait AbstractWorkflowTransition
+{
+	public function startsFromAnyStateId()
+	{
+		return false;
+	}
+
+	/**
+	 * @param WorkflowContext $context
+	 *
+	 * @return boolean
+	 *
+	 * true - guard condition is met - we can run this transition
+	 * false - transition will not be run
+	 *
+	 * @Workflow-Guard None
+	 */
+	public function checkGuardCondition($context)
+	{
+		// no guard check, by default
+		return true;
+	}
+
+	/**
+	 * Code to run when this transition is triggered.
+	 *
+	 * @param WorkflowContext $context
+	 *
+	 * @return void
+	 *
+	 * @Workflow-Action None
+	 */
+	public function run($context)
+	{
+	}
+}
